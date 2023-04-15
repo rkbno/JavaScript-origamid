e1c50faf9c9d338afb3e093a7bae5612697e71d2
rk <103226779+rkbno@users.noreply.github.com>
2023-04-14 22:30:29 -0300

    estudos

--- js-p/-iniciantes/arrays-e-loops/array-e-loops.js
+++ js-p/-iniciantes/arrays-e-loops/array-e-loops.js
@@ -36,11 +36,24 @@ var brasilGanhouACopa = [1959, 1962, 1970, 1994, 2002]
 // Interaja com a array utilizando um loop, para mostrar
 // no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
 brasilGanhouACopa.forEach((ano) => {
-  console.log(`O brasil ganhou a copa no ano de ${ano}`)
+  console.log(`O brasil ganhou a copa de ${ano}`)
 })
 
 // Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
 var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
 
+
+for (var fruta = 0; fruta < frutas.length; fruta++){
+  console.log(frutas[fruta])
+  if (frutas[fruta] === 'Pera'){
+    break;
+  }
+  console.log(frutas[fruta])
+}
+
 // Coloque a última fruta da array acima em uma variável,
 // sem remover a mesma da array.
+var ultima = frutas[frutas.length - 1]
+console.log(ultima)
+
+
