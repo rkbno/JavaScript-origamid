b51dc6d50fc8a56c54b5b174d1b576134a611dd7
rk <103226779+rkbno@users.noreply.github.com>
2023-06-25 20:06:29 -0300

    estudos

--- array-interacao/exercicios.js
+++ array-interacao/exercicios.js
@@ -1,68 +1,82 @@
-const comidas = ['Pizza', 'Frango', 'Carne', 'Macarrão'];
-// Remova o primeiro valor de comidas e coloque em uma variável
-// Remova o último valor de comidas e coloque em uma variável
-// Adicione 'Arroz' ao final da array
-// Adicione 'Peixe' e 'Batata' ao início da array
+// Selecione cada curso e retorne uma array
+// com objetos contendo o título, descricao,
+// aulas e horas de cada curso
+const aulas = [
+  {
+    nome: 'HTML 1',
+    min: 15
+  },
+  {
+    nome: 'HTML 2',
+    min: 10
+  },
+  {
+    nome: 'CSS 1',
+    min: 20
+  },
+  {
+    nome: 'JS 1',
+    min: 25
+  },
+]
 
-const primeiroValor = comidas.shift();
-const ultimoValor = comidas.pop();
-comidas.push('Arroz');
-comidas.unshift('Peixe');
-
-
-
-console.log(comidas);
-console.log(primeiroValor);
-console.log(ultimoValor);
-
-
-
-
-const estudantes = ['Marcio', 'Brenda', 'Joana', 'Kleber', 'Julia'];
-// Arrume os estudantes em ordem alfabética
-// Inverta a ordem dos estudantes
-// Verifique se Joana faz parte dos estudantes
-// Verifique se Juliana faz parte dos estudantes
-estudantes.sort();
-estudantes.reverse();
-
-
-console.log(estudantes);
-console.log(estudantes.includes('Joana'));
-console.log(estudantes.includes('Juliana'));
+aulas.forEach((item) => { 
+  console.log(item);
+});
 
 
 
 
+// Retorne uma lista com os
+// números maiores que 100
+const numeros = [3, 44, 333, 23, 122, 322, 33];
 
-let html = `<section>
-              <div>Sobre</div>
-              <div>Produtos</div>
-              <div>Contato</div>
-            </section>`
-// Substitua section por ul e div com li,
-// utilizando split e join
+const maiorQue100 = numeros.filter(n => n > 100);
 
-
-html = html.split('section');
-html = html.join('ul');
-html = html.split('div');
-html = html.join('li');
+console.log(maiorQue100);
+console.log(numeros);
 
 
-console.log(html);
+// Verifique se Baixo faz parte
+// da lista de instrumentos e retorne true
+const instrumentos = ['Guitarra', 'Baixo', 'Bateria', 'Teclado']
+
+const temInstrumentos = instrumentos.some(instrumento => instrumento === 'Baixo');
+
+console.log(temInstrumentos);
 
 
+// Retorne o valor total das compras
+const compras = [
+  {
+    item: 'Banana',
+    preco: 'R$ 4,99'
+  },
+  {
+    item: 'Ovo',
+    preco: 'R$ 2,99'
+  },
+  {
+    item: 'Carne',
+    preco: 'R$ 25,49'
+  },
+  {
+    item: 'Refrigerante',
+    preco: 'R$ 5,35'
+  },
+  {
+    item: 'Quejo',
+    preco: 'R$ 10,60'
+  }
+]
 
-
-const carros = ['Ford', 'Fiat', 'VW', 'Honda'];
-// Remova o último carro, mas antes de remover
-// salve a array original em outra variável
-const arrayOg = carros.slice();
-
-
-carros.pop();
-
-
-console.log(carros);
-console.log(arrayOg);
+const newItens = compras.map((item) => {
+  const valor = +item.preco.replace('R$', '').trim().replace(',','.');
+  const total = valor.reduce((acumulador, atual) => {
+    return acumulador + atual;
+  });
+  
+  
+  
+  console.log(total);
+});
